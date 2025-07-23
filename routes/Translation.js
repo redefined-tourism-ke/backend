77342606180eef00291c14f352de70e11ec28576
wakoTheDev <wakopaul19@gmail.com>
2025-07-24 00:44:25 +0300

    updates

--- routes/Translation.js
+++ routes/Translation.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const translationController = require('../controllers/translationController');
+const translationController = require('../controllers/TranslationController');
 const { authenticate } = require('../middleware/auth');
 
 router.post('/', authenticate, translationController.translateText);
