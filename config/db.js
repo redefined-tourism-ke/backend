a9d5858d050483675abe99d18eea91b6ea530086
wakoTheDev <wakopaul19@gmail.com>
2025-07-24 00:51:54 +0300

    updates

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/tamu-africa', {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mongodbuser:mongodbuser@cluster0.fzicuak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
